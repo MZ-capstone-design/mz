27202ea249536734ad052b8ff45a2906083feff5
MinWook6457 <minuk6457@gmail.com>
2024-07-28 19:16:58 +0900

    [기능추가] : 크롤링 후 description 부분 <b> 태그 삭제 => cheerio 사용

--- server/api/crawl/service.crawl/getCurrentFiveNews.js
+++ server/api/crawl/service.crawl/getCurrentFiveNews.js
@@ -1,13 +1,14 @@
 const axios = require('axios');
+const cheerio = require('cheerio');
 require('dotenv').config();
 
 module.exports = async () => {
-    try{
+    try {
         const clientID = process.env.NAVER_CLIENT_ID;
         const clientSecret = process.env.NAVER_CLIENT_SECRET;
-      
+
         const query = '몽타주';
-        
+
         const apiUrl = `https://openapi.naver.com/v1/search/news.json?query=${encodeURIComponent(query)}&display=5&sort=date`;
 
         const response = await axios.get(apiUrl, {
@@ -17,14 +18,18 @@ module.exports = async () => {
             },
         });
 
-        console.log(response.data);
-
-        console.log(response);
-
         const articles = response.data.items;
+
+        // <b> 태그를 제거하는 부분
+        articles.forEach(article => {
+            const $ = cheerio.load(article.description);
+            article.description = $.text();
+        });
+
+        console.log(articles);
         return articles;
 
-    }catch(err){
+    } catch (err) {
         console.error(err);
     }
 };
